aaa03d384012ce5181844ee80ee53b2128046de7
binforkoreait <seongubabo1@gmail.com>
2026-09-03 12:08:15 +0900

    dddd

--- 26311013_BaekGyeongBin/Game.cpp
+++ 26311013_BaekGyeongBin/Game.cpp
@@ -1,19 +1,22 @@
-﻿﻿// link the 2d game library
+﻿// link the 2d game library
 #if defined(_DEBUG)
-#if defined(_M_X64) // 64-bit 아키텍처
+
+#if defined(_M_X64)
 #pragma comment(lib, "glc2d_x64_debug.lib")
-#elif defined(_M_IX86) // 32-bit 아키텍처
+#elif defined(_M_IX86)
 #pragma comment(lib, "glc2d_win32_debug.lib")
 #endif
+
 #else
+
 #if defined(_M_X64)
 #pragma comment(lib, "glc2d_x64_release.lib")
 #elif defined(_M_IX86)
 #pragma comment(lib, "glc2d_win32_release.lib")
 #endif
+
 #endif
 
-// include the 2d game header file
 #include "glc2d.h"
 #include "CApplication.h"
 
@@ -21,9 +24,11 @@ CApplication g_app;
 
 int main()
 {
-	g_app.Init();
-	g2_Run();
-	g_app.Destroy();
+    g_app.Init();
 
-	return 0;
+    g2_Run();
+
+    g_app.Destroy();
+
+    return 0;
 }
